564a81e574c9e8e4f49090df4ec950f3aaabcdc6
nam <namgd69@gmail.com>
2021-11-10 14:21:48 +0700

    hanghoa project

---
+++ src/main/java/com/example/hanghoa/HanghoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanghoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghoaApplication.class, args);
+	}
+
+}
